61b07029abbba943b3b5d7c32f065b45c28855f6
oesel <oesel@yandex.ru>
2023-02-13 01:13:55 +0300

    code added to task15

---
+++ seminar2_task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// Приглашение ко вводу
+// Перевоз строкового значения в целочисленное
+Console.Write("Введите число дня недели, проверьте это выходной: ");
+int numDayofWeek = Convert.ToInt32(Console.ReadLine());
+// Решение с помощью swith-кейс и вывод результата в терминал
+switch (numDayofWeek)
+{
+    case 1: Console.WriteLine("нет"); break;
+    case 2: Console.WriteLine("нет"); break;
+    case 3: Console.WriteLine("нет"); break;
+    case 4: Console.WriteLine("нет"); break;
+    case 5: Console.WriteLine("нет"); break;
+    case 6: Console.WriteLine("да, это выходной"); break;
+    case 7: Console.WriteLine("да, это выходной"); break;
+    default: Console.WriteLine("это не день недели!"); break;
+    Console.WriteLine(numDayofWeek);
+}
