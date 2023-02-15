fa9b179024566426e5cedc7a81890c27e9927d4a
oesel <oesel@yandex.ru>
2023-02-15 20:38:28 +0300

    code added to task19

---
+++ seminar3_task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите ПЯТИЗНАЧНОЕ число -> ");
+string? num = Console.ReadLine();
+int L = num!.Length;
+
+if (L==5)
+{
+    if(num[0]==num[4]&&num[1]==num[3])
+    {
+        System.Console.WriteLine($"{num} ПАЛИНДРОМ");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num} НЕ ПАЛИНДРОМ");
+    }
+}
+else
+{
+    System.Console.WriteLine($"{num} НЕ ПЯТИЗНАЧНОЕ число");
+}
