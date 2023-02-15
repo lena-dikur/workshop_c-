540c0f13b878c33f127bd274276219ca4d31a4d2
oesel <oesel@yandex.ru>
2023-02-15 21:45:59 +0300

    revised the decision task13

--- seminar2_task13/Program.cs
+++ seminar2_task13/Program.cs
@@ -3,24 +3,40 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Write("Пожалуйста, задайте любое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num > 99 && num < 1000)
+// {
+//     int numresult = num%10;
+//     Console.WriteLine($"Третья цифра заданного числа: {numresult}");
+// }
+// else if (num > 999 && num < 10000)
+// {
+//     int numresult = (num%100)/10;
+//     Console.WriteLine($"Третья цифра заданного числа: {numresult}");
+// }
+// else if (num > 9999 && num < 100000)
+// {
+//     int numresult = (num%1000)/100;
+//     Console.WriteLine($"Третья цифра заданного числа: {numresult}");
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
 Console.Write("Пожалуйста, задайте любое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num > 99 && num < 1000)
+
+if(num < 100)
 {
-    int numresult = num%10;
-    Console.WriteLine($"Третья цифра заданного числа: {numresult}");
-}
-else if (num > 999 && num < 10000)
-{
-    int numresult = (num%100)/10;
-    Console.WriteLine($"Третья цифра заданного числа: {numresult}");
-}
-else if (num > 9999 && num < 100000)
-{
-    int numresult = (num%1000)/100;
-    Console.WriteLine($"Третья цифра заданного числа: {numresult}");
+    Console.WriteLine("Третьей цифры нет ");
 }
 else
 {
-    Console.WriteLine("Третьей цифры нет");
+    while(num>999)
+    {
+        num/=10;
+    }
+Console.WriteLine(num%10);
 }
