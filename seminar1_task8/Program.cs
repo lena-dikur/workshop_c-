130c8876301e1003a698990fd2c681cf3594810e
oesel <oesel@yandex.ru>
2023-02-15 21:45:23 +0300

    revised the decision task8

--- seminar1_task8/Program.cs
+++ seminar1_task8/Program.cs
@@ -2,28 +2,39 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// Приглашение ко вводу
-// Перевод строковых в целочисленные
-Console.Write("Пожалуйста, введите число N: ");
+// // Приглашение ко вводу
+// // Перевод строковых в целочисленные
+// Console.Write("Пожалуйста, введите число N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// // Создана новая переменная
+// int num1 = 2;
+// // Создана пустая строка
+// string result = string.Empty;
+// // Накапливание результата с помощью оператора while, значения увеличиваются на 2
+// while (num > num1)
+// {
+//     result = result + num1 + " ";
+//     num1 = num1 + 2;
+// }
+// // Проверка на четность и необходимость включения введенного числа
+// // Вывод строки в консоль
+// if (num % 2 == 0)
+// {
+//     result = result + num;
+//     Console.WriteLine("Все четные числа от 1 до N: " + result);
+// }
+// else
+// {
+//     Console.WriteLine("Все четные числа от 1 до N: " + result);
+// }
+
+Console.Write("Пожалуйста, введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-// Создана новая переменная
-int num1 = 2;
-// Создана пустая строка
-string result = string.Empty;
-// Накапливание результата с помощью оператора while, значения увеличиваются на 2
-while (num > num1)
+int num1 = 1;
+while(num1<=num)
 {
-    result = result + num1 + " ";
-    num1 = num1 + 2;
+    if(num1%2==0)
+    Console.Write(num1+", ");
+    num1++;
 }
-// Проверка на четность и необходимость включения введенного числа
-// Вывод строки в консоль
-if (num % 2 == 0)
-{
-    result = result + num;
-    Console.WriteLine("Все четные числа от 1 до N: " + result);
-}
-else
-{
-    Console.WriteLine("Все четные числа от 1 до N: " + result);
-}
+
